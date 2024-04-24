740deff3534e52870711c8dec587788158ffd1ce
Nicolas Pereira <41456803+hqnicolas@users.noreply.github.com>
2024-04-24 10:39:01 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
+sudo apt-get install armbian-config
+sudo armbian-config
+# SYSTEM
+# FREEZE
+# KERNEL
 wget https://raw.githubusercontent.com/hqnicolas/pop_os_arm64/main/install_pop_os_arm64.sh
 sudo chmod 777 install_pop_os_arm64.sh
 sudo ./install_pop_os_arm64.sh
