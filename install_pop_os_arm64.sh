fdf49ad71e4cf0050961c66eeb7307d34126cb63
Nicolas Pereira <41456803+hqnicolas@users.noreply.github.com>
2024-04-24 08:03:08 -0300

    Update install_pop_os_arm64.sh

--- install_pop_os_arm64.sh
+++ install_pop_os_arm64.sh
@@ -1,5 +1,42 @@
 #!/bin/bash
 
+# Verificando se o usuário deseja congelar ou derreter os pacotes
+if dialog --title "Congelar / Derreter Pacotes" --backtitle "$BACKTITLE" --yes-label "$1" --no-label "Cancelar" --yesno "\nDeseja congelar ou derreter os pacotes do Armbian?" 7 54; then
+    # Conferindo se o gerenciador de pacotes está em execução
+    if ! is_package_manager_running; then
+        # Executando as ações conforme descrito no script
+        TARGET_BRANCH=$BRANCH
+        exceptions "$BRANCH"
+        unset PACKAGE_LIST
+        
+        # Pacotes básicos
+        check_if_installed linux-u-boot-${BOARD}-${UBOOT_BRANCH} && PACKAGE_LIST=$PACKAGE_LIST" linux-u-boot-${BOARD}-${UBOOT_BRANCH}"
+        check_if_installed linux-image${TARGET_BRANCH}-${TARGET_FAMILY} && PACKAGE_LIST=$PACKAGE_LIST" linux-image${TARGET_BRANCH}-${TARGET_FAMILY}"
+        check_if_installed linux-dtb$TARGET_BRANCH-$TARGET_FAMILY && PACKAGE_LIST=$PACKAGE_LIST" linux-dtb$TARGET_BRANCH-$TARGET_FAMILY"
+        
+        # Pacotes antigos do BSP
+        check_if_installed linux-$(lsb_release -cs)-root$TARGET_BRANCH-$BOARD && PACKAGE_LIST=$PACKAGE_LIST" linux-$(lsb_release -cs)-root$TARGET_BRANCH-$BOARD"
+        check_if_installed linux-headers${TARGET_BRANCH}-${TARGET_FAMILY} && PACKAGE_LIST=$PACKAGE_LIST" linux-headers${TARGET_BRANCH}-${TARGET_FAMILY}"
+        
+        # Pacotes novos do BSP
+        check_if_installed armbian-${LINUXFAMILY} && PACKAGE_LIST=$PACKAGE_LIST" armbian-${LINUXFAMILY}"
+        check_if_installed armbian-${BOARD} && PACKAGE_LIST=$PACKAGE_LIST" armbian-${BOARD}"
+        check_if_installed armbian-$(lsb_release -cs) && PACKAGE_LIST=$PACKAGE_LIST" armbian-$(lsb_release -cs)"
+        check_if_installed armbian-$(lsb_release -cs)-desktop-xfce && PACKAGE_LIST=$PACKAGE_LIST" armbian-$(lsb_release -cs)-desktop-xfce"
+        check_if_installed armbian-firmware && PACKAGE_LIST=$PACKAGE_LIST" armbian-firmware"
+        check_if_installed armbian-firmware-full && PACKAGE_LIST=$PACKAGE_LIST" armbian-firmware-full"
+        
+        local words=( $PACKAGE_LIST )
+        local command="unhold"
+        IFS=" "
+        [[ $1 == "Freeze" ]] && local command="hold"
+        for word in $PACKAGE_LIST; do apt-mark $command $word; done | dialog --backtitle "$BACKTITLE" --title "Pacotes ${1,,}" --progressbox $((${#words[@]}+2)) 64
+    fi
+else
+    # Cancelar a operação
+    echo "Operação cancelada pelo usuário."
+fi
+
 # Update system
 sudo apt update -y && sudo apt full-upgrade -y
 
