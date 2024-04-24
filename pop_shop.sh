d86da4981484eea2f1b528ca7019d06eee6abd48
Nicolas Pereira <41456803+hqnicolas@users.noreply.github.com>
2024-04-24 10:23:49 -0300

    Update pop_shop.sh

--- pop_shop.sh
+++ pop_shop.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 sudo apt install git
+sudo add-apt-repository ppa:apandada1/libhandy-1
 git clone https://github.com/pop-os/shop.git
 sudo apt-get install gettext libappstream-dev libgee-0.8-dev libgranite-dev libgtk-3-dev libjson-glib-dev libpackagekit-glib2-dev libsoup2.4-dev libunity-dev libxml2-dev libxml2-utils repoman meson valac make build-essential cmake libhandy-0.0-dev libflatpak-dev flatpak
-cd shop 
-sudo add-apt-repository ppa:apandada1/libhandy-1
-sudo apt-get update
+sudo ninja install
 sudo apt install libhandy-1-dev
 sudo apt install libpolkit-gobject-1-dev
+cd shop 
 meson build --prefix=/usr
 cd build
-sudo apt install generate-ninja
-gn gen out/Default
-sudo ninja install
 ninja
 # Reboot the system
 sudo reboot
