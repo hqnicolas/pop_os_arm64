51f6ee935548e55f223bf38bf4241bb0e1d95b3a
Nicolas Pereira <41456803+hqnicolas@users.noreply.github.com>
2024-04-24 10:31:40 -0300

    Update pop_shop.sh

--- pop_shop.sh
+++ pop_shop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt install git
 sudo add-apt-repository ppa:apandada1/libhandy-1
-git clone https://github.com/pop-os/shop.git
+git clone -branch master_jammy https://github.com/pop-os/shop.git
 sudo apt-get install gettext libappstream-dev libgee-0.8-dev libgranite-dev libgtk-3-dev libjson-glib-dev libpackagekit-glib2-dev libsoup2.4-dev libunity-dev libxml2-dev libxml2-utils repoman meson valac make build-essential cmake libhandy-0.0-dev libflatpak-dev flatpak
 sudo ninja install
 sudo apt install libhandy-1-dev
