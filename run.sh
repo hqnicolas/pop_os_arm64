c23cf60b17460c9b37b1859cf718d914b2cd5afa
Nicolas Pereira <41456803+hqnicolas@users.noreply.github.com>
2024-04-24 10:34:31 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 wget https://raw.githubusercontent.com/hqnicolas/pop_os_arm64/main/install_pop_os_arm64.sh
 sudo chmod 777 install_pop_os_arm64.sh
 sudo ./install_pop_os_arm64.sh
+wget https://raw.githubusercontent.com/hqnicolas/pop_os_arm64/main/install_wayland_browser.sh
+sudo chmod 777 install_wayland_browser.sh
+sudo ./install_wayland_browser.sh
