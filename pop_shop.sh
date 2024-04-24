2294bd16cc8dc7d3a251ee1f45afd514140b0cdd
Nicolas Pereira <41456803+hqnicolas@users.noreply.github.com>
2024-04-24 10:13:22 -0300

    Update pop_shop.sh

--- pop_shop.sh
+++ pop_shop.sh
@@ -2,9 +2,6 @@
 sudo apt install git
 git clone https://github.com/pop-os/shop.git
 sudo apt-get install gettext libappstream-dev libgee-0.8-dev libgranite-dev libgtk-3-dev libjson-glib-dev libpackagekit-glib2-dev libsoup2.4-dev libunity-dev libxml2-dev libxml2-utils repoman meson valac make build-essential cmake libhandy-0.0-dev libflatpak-dev flatpak
-git clone https://github.com/pop-os/repoman.git
-cd repoman
-sudo python3 setup.py install   
 cd shop 
 meson build --prefix=/usr
 cd build
