7988cbc1fa94d76534fe661e7536337d7de86fce
Nicolas Pereira <41456803+hqnicolas@users.noreply.github.com>
2024-04-26 16:49:29 -0300

    Update install_wayland_browser.sh

--- install_wayland_browser.sh
+++ install_wayland_browser.sh
@@ -15,14 +15,10 @@ sudo sed -i 's/WaylandEnable=false/WaylandEnable=true/' /etc/gdm3/custom.conf
 
 sudo sed -i 's/exit 0$/echo dec > \/dev\/video-dec0\nchown root:video \/dev\/video-dec0\nchmod 0660 \/dev\/video-dec0\necho enc > \/dev\/video-enc0\nchown root:video \/dev\/video-enc0\nchmod 0660 \/dev\/video-enc0\nexit 0/g' /etc/rc.local
 
-
-#!/bin/bash
-
 echo -e "# Add multimedia group permissions\n" > /etc/udev/rules.d/11-rockchip-multimedia.rules
 echo -e "KERNEL==\"mpp_service\", MODE=\"0660\", GROUP=\"video\"\n" >> /etc/udev/rules.d/11-rockchip-multimedia.rules
 echo -e "KERNEL==\"rga\", MODE=\"0660\", GROUP=\"video\"\n" >> /etc/udev/rules.d/11-rockchip-multimedia.rules
 echo -e "KERNEL==\"system-dma32\", MODE=\"0666\", GROUP=\"video\"\n" >> /etc/udev/rules.d/11-rockchip-multimedia.rules
 echo -e "KERNEL==\"system-uncached-dma32\", MODE=\"0666\", GROUP=\"video\"RUN+=\"/usr/bin/chmod a+rw /dev/dma_heap\"\n" >> /etc/udev/rules.d/11-rockchip-multimedia.rules
-
 
 $ sudo reboot
