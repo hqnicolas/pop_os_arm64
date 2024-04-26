375f87066edcc9d1bb1563f88993fb92b9bb9b87
Nicolas Pereira <41456803+hqnicolas@users.noreply.github.com>
2024-04-26 17:54:19 -0300

    Delete ffmpeg.sh

--- ffmpeg.sh
+++
@@ -1,6 +0,0 @@
-sudo wget http://apt.undo.it:7241/apt.undo.it.asc -O /etc/apt/trusted.gpg.d/apt.undo.it.asc
-echo "deb http://apt.undo.it:7241/ubuntu jammy main" | sudo tee /etc/apt/sources.list.d/apt.undo.it.list
-sudo apt update
-sudo apt install ffmpeg-v4l2request mpv
-sudo mkdir -p /etc/mpv
-echo -e "hwdec=drm\ndrm-drmprime-video-plane=primary\ndrm-draw-plane=overlay" | sudo tee /etc/mpv/mpv.conf
