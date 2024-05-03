a7d6633039266a9d2abee656cd38a928496fa473
Nicolas Pereira <41456803+hqnicolas@users.noreply.github.com>
2024-05-03 01:50:15 -0300

    Update install_wayland_browser.sh

--- install_wayland_browser.sh
+++ install_wayland_browser.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+sudo add-apt-repository ppa:liujianfeng1994/panfork-mesa
+sudo add-apt-repository ppa:liujianfeng1994/rockchip-multimedia
 sudo apt update
 sudo apt dist-upgrade
-sudo apt install libv4l-rkmpp -y
+sudo apt install panfork-mesa rockchip-multimedia-config libv4l-rkmpp -y
+sudo apt update
+sudo apt dist-upgrade
 sudo apt install v4l-utils -y
 sudo apt install ffmpeg -y
 sudo apt install rockchip-multimedia-config -y
