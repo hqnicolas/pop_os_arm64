5511a82fea3c38e3b4875aabdaad894f20e61f6b
Nicolas Pereira <41456803+hqnicolas@users.noreply.github.com>
2024-06-26 11:02:00 -0300

    Update install_pop_os_arm64.sh

--- install_pop_os_arm64.sh
+++ install_pop_os_arm64.sh
@@ -45,5 +45,21 @@ sudo mv /boot/vmlinuz.old /boot/vmlinuz
 sudo mv /boot/uInitrd /boot/uInitrd.bkp
 sudo ln -s /boot/uInitrd-$(uname -r) /boot/uInitrd
 
+sudo apt install glmark2-es2-wayland -y
+# glmark2-es2-wayland
+
+# Edit the file /etc/gdm3/custom.conf 
+# WaylandEnable=true 
+
+sudo sed -i 's/WaylandEnable=false/WaylandEnable=true/' /etc/gdm3/custom.conf
+
+sudo sed -i 's/exit 0$/echo dec > \/dev\/video-dec0\nchown root:video \/dev\/video-dec0\nchmod 0660 \/dev\/video-dec0\necho enc > \/dev\/video-enc0\nchown root:video \/dev\/video-enc0\nchmod 0660 \/dev\/video-enc0\nexit 0/g' /etc/rc.local
+
+echo -e "# Add multimedia group permissions\n" > /etc/udev/rules.d/11-rockchip-multimedia.rules
+echo -e "KERNEL==\"mpp_service\", MODE=\"0660\", GROUP=\"video\"\n" >> /etc/udev/rules.d/11-rockchip-multimedia.rules
+echo -e "KERNEL==\"rga\", MODE=\"0660\", GROUP=\"video\"\n" >> /etc/udev/rules.d/11-rockchip-multimedia.rules
+echo -e "KERNEL==\"system-dma32\", MODE=\"0666\", GROUP=\"video\"\n" >> /etc/udev/rules.d/11-rockchip-multimedia.rules
+echo -e "KERNEL==\"system-uncached-dma32\", MODE=\"0666\", GROUP=\"video\"RUN+=\"/usr/bin/chmod a+rw /dev/dma_heap\"\n" >> /etc/udev/rules.d/11-rockchip-multimedia.rules
+
 # Reboot the system
 sudo reboot
