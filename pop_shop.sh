e031b73fc6a11a2d68b0a16a501f866d04ebaa06
Nicolas Pereira <41456803+hqnicolas@users.noreply.github.com>
2024-06-26 12:56:55 -0300

    Update pop_shop.sh

--- pop_shop.sh
+++ pop_shop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt install git -y
-sudo add-apt-repository ppa:apandada1/libhandy-1
+sudo add-apt-repository ppa:apandada1/libhandy-1 -y
 sudo apt-get install gettext libappstream-dev libgee-0.8-dev libgranite-dev libgtk-3-dev libjson-glib-dev libpackagekit-glib2-dev libsoup2.4-dev libunity-dev libxml2-dev libxml2-utils repoman meson valac make build-essential cmake libhandy-0.0-dev libflatpak-dev flatpak -y
 apt-get install meson libhandy-1-dev libgee-0.8-2 libpolkit-gobject-1-dev libsoup2.4-dev gnome-software-plugin-flatpak -y
 git clone https://github.com/pop-os/repoman.git
@@ -16,5 +16,6 @@ sudo io.elementary.appcenter --gapplication-replace
 sudo apt install snapd -y
 sudo snap install snap-store
 sudo snap install ms-365-electron
+sudo add-apt-repository -r ppa:apandada1/libhandy-1 -y
 # Reboot the system
 sudo reboot
