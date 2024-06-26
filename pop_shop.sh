f01937f30d5b2c8f8ed8a808286775cc20b5cdb0
Nicolas Pereira <41456803+hqnicolas@users.noreply.github.com>
2024-06-26 14:15:47 -0300

    Update pop_shop.sh

--- pop_shop.sh
+++ pop_shop.sh
@@ -2,7 +2,7 @@
 sudo apt install git -y
 sudo add-apt-repository ppa:apandada1/libhandy-1 -y
 sudo apt-get install gettext libappstream-dev libgee-0.8-dev libgranite-dev libgtk-3-dev libjson-glib-dev libpackagekit-glib2-dev libsoup2.4-dev libunity-dev libxml2-dev libxml2-utils repoman meson valac make build-essential cmake libhandy-0.0-dev libflatpak-dev flatpak -y
-apt-get install libgranite-dev elementary-sdk valac meson libhandy-1-dev libgee-0.8-2 libpolkit-gobject-1-dev libsoup2.4-dev gnome-software-plugin-flatpak -y
+apt-get install libappstream-glib-dev libgranite-dev elementary-sdk valac meson libhandy-1-dev libgee-0.8-2 libpolkit-gobject-1-dev libsoup2.4-dev gnome-software-plugin-flatpak -y
 git clone https://github.com/pop-os/repoman.git
 cd repoman
 sudo python3 setup.py install
