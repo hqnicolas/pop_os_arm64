a08fac9ca50820e23a618db089ea9656c88dc8aa
Nicolas Pereira <41456803+hqnicolas@users.noreply.github.com>
2024-06-26 07:59:59 -0300

    Update install_pop_os_arm64.sh

--- install_pop_os_arm64.sh
+++ install_pop_os_arm64.sh
@@ -15,15 +15,17 @@ sudo apt-get remove gdm -y
 sudo apt purge --autoremove ubuntu-desktop -y
 
 # Add the Pop OS ISO signing key
-sudo gpg --keyserver keyserver.ubuntu.com --recv-keys 63C46DF0140D738961429F4E204DD8AEC33A7AFF
-sudo gpg --export 63C46DF0140D738961429F4E204DD8AEC33A7AFF > pop_os.gpg
-sudo mv pop_os.gpg /etc/apt/trusted.gpg.d/
-sudo chown root:root /etc/apt/trusted.gpg.d/pop_os.gpg
-sudo chmod 644 /etc/apt/trusted.gpg.d/pop_os.gpg
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 63C46DF0140D738961429F4E204DD8AEC33A7AFF
+
+
 
 # Add the Pop OS APT repositories
 sudo add-apt-repository "deb http://apt.pop-os.org/release $(lsb_release -cs) main" -y
-sudo add-apt-repository "deb http://apt.pop-os.org/staging/master $(lsb_release -cs) main" -y
+sudo add-apt-repository "deb http://apt.pop-os.org/proprietary $(lsb_release -cs) main" -y
+sudo add-apt-repository "deb http://apt.pop-os.org/ubuntu $(lsb_release -cs) main multiverse restricted universe" -y
+sudo add-apt-repository "deb http://apt.pop-os.org/ubuntu $(lsb_release -cs)-security main multiverse restricted universe" -y
+sudo add-apt-repository "deb http://apt.pop-os.org/ubuntu $(lsb_release -cs)-updates main multiverse restricted universe" -y
+sudo add-apt-repository "deb http://apt.pop-os.org/ubuntu $(lsb_release -cs)-proposed main multiverse restricted universe" -y
 
 # Update system again
 sudo apt update -y && sudo apt full-upgrade -y
@@ -36,12 +38,6 @@ sudo apt purge --autoremove linux*-raspi* -y
 
 #Fix the Pop-OS APT sources
 sudo sed -i 's/Enabled: yes/Enabled: no/g' /etc/apt/sources.list.d/system.sources /etc/apt/sources.list.d/pop-os-apps.sources
-
-# add system76 ppa
-sudo add-apt-repository ppa:system76/pop -y
-
-# Update system again
-sudo apt update -y && sudo apt full-upgrade -y
 
 # install desktop from system76 ppa
 sudo apt install pop-desktop
